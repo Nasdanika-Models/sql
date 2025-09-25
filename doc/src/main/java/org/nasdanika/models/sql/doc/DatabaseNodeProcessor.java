85130a461772287511cb79eb41f13b49e52764e7
Pavel Vlasov <Pavel.Vlasov@nasdanika.org>
2025-09-24 21:17:48 -0400

    Column and data type properties and tables

--- doc/src/main/java/org/nasdanika/models/sql/doc/DatabaseNodeProcessor.java
+++ doc/src/main/java/org/nasdanika/models/sql/doc/DatabaseNodeProcessor.java
@@ -5,10 +5,14 @@ import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 
+import org.eclipse.emf.ecore.EAttribute;
+import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.EReference;
 import org.nasdanika.common.Context;
 import org.nasdanika.common.DocumentationFactory;
+import org.nasdanika.common.ENamedElementComparator;
 import org.nasdanika.common.ProgressMonitor;
+import org.nasdanika.common.Util;
 import org.nasdanika.graph.emf.EReferenceConnection;
 import org.nasdanika.graph.processor.NodeProcessorConfig;
 import org.nasdanika.models.app.Action;
@@ -109,6 +113,23 @@ public class DatabaseNodeProcessor extends ModelElementNodeProcessor<Database> {
 			if (label instanceof Action) {										
 				DynamicTableBuilder<Entry<EReferenceConnection, WidgetFactory>> dataTypesTableBuilder = new DynamicTableBuilder<>("nsd-table");
 				buildNamedElementColumns(dataTypesTableBuilder, progressMonitor);
+				for (EAttribute attr: SqlPackage.Literals.DATA_TYPE.getEAttributes().stream().sorted(ENamedElementComparator.INSTANCE).toList()) {
+					dataTypesTableBuilder.addStringColumnBuilder(
+							attr.getName(), 
+							true, 
+							true, 
+							Util.nameToLabel(attr.getName()), 
+							endpoint -> {
+								EObject eTarget = endpoint.getKey().getTarget().get();
+								if (eTarget.eIsSet(attr)) {
+									Object val = eTarget.eGet(attr);
+									return String.valueOf(val);							
+								}
+								
+								return "";
+							});
+				}
+				
 				
 				org.nasdanika.models.html.Tag dataTypesTable = dataTypesTableBuilder.build(
 						referenceOutgoingEndpoints.stream().sorted((a,b) -> {
@@ -124,7 +145,6 @@ public class DatabaseNodeProcessor extends ModelElementNodeProcessor<Database> {
 			}
 		}
 	}
-	
 	
 //	@OutgoingReferenceBuilder(
 //			nsURI = SqlPackage.eNS_URI,
--- doc/src/main/java/org/nasdanika/models/sql/doc/TableNodeProcessor.java
+++ doc/src/main/java/org/nasdanika/models/sql/doc/TableNodeProcessor.java
@@ -5,10 +5,14 @@ import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 
+import org.eclipse.emf.ecore.EAttribute;
+import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.EReference;
 import org.nasdanika.common.Context;
 import org.nasdanika.common.DocumentationFactory;
+import org.nasdanika.common.ENamedElementComparator;
 import org.nasdanika.common.ProgressMonitor;
+import org.nasdanika.common.Util;
 import org.nasdanika.graph.emf.EReferenceConnection;
 import org.nasdanika.graph.processor.NodeProcessorConfig;
 import org.nasdanika.models.app.Action;
@@ -64,7 +68,38 @@ public class TableNodeProcessor extends ModelElementNodeProcessor<Table> {
 			if (label instanceof Action) {										
 				DynamicTableBuilder<Entry<EReferenceConnection, WidgetFactory>> columnsTableBuilder = new DynamicTableBuilder<>("nsd-table");
 				buildNamedElementColumns(columnsTableBuilder, progressMonitor);
+				// Does not work this way
+//				columnsTableBuilder.addStringColumnBuilder(
+//						"type", 
+//						true, 
+//						true, 
+//						"Type", 
+//						endpoint -> {
+//							WidgetFactory typeWidgetFactory = ((ColumnNodeProcessor) endpoint.getValue()).getTypeWidgetFactory();
+//							if (typeWidgetFactory != null) {
+//								return typeWidgetFactory.createLinkString(progressMonitor);
+//							}
+//							
+//							return "";
+//						});
 				
+				for (EAttribute attr: SqlPackage.Literals.COLUMN.getEAttributes().stream().sorted(ENamedElementComparator.INSTANCE).toList()) {
+					columnsTableBuilder.addStringColumnBuilder(
+							attr.getName(), 
+							true, 
+							true, 
+							Util.nameToLabel(attr.getName()), 
+							endpoint -> {
+								EObject eTarget = endpoint.getKey().getTarget().get();
+								if (eTarget.eIsSet(attr)) {
+									Object val = eTarget.eGet(attr);
+									return String.valueOf(val);							
+								}
+								
+								return "";
+							});
+				}
+								
 				org.nasdanika.models.html.Tag columnsTable = columnsTableBuilder.build(
 						referenceOutgoingEndpoints.stream().sorted((a,b) -> {
 							NamedElement ane = (NamedElement) a.getKey().getTarget().get();
